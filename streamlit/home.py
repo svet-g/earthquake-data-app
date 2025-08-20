3add84d2f173662b2fecb01713a5b5536f89b902
svet-g <svetlana.gljadelkina@gmail.com>
2025-08-20 16:16:02 +0100

    set up streamlit folder structure

---
+++ streamlit/home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+def home():
+    st.title("earthquake analysis")
+    
+if __name__ == '__main__':
+    home()
---
+++ streamlit/pages/graphs.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def graphs():
+    st.title("earthquake analysis")
+    
+if __name__ == '__main__':
+    graphs()
