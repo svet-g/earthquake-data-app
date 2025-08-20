6e20fedb5b0551fcd819163d3cd83fcd80a5edf6
svet-g <svetlana.gljadelkina@gmail.com>
2025-08-20 15:13:53 +0100

    feature: add a data cache from initial extraction

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -1,12 +1,22 @@
+import os
+from pathlib import Path
+import geopandas
 from utils.extract_utils import db_engine, get_table, recreate_geometry
 
+cache_path = Path(__file__).parent.parent.parent / 'data' / 'cache' / 'earthquakes.parquet'
+
 def extract():
-    engine = db_engine()
-    table_name = 'earthquakes-svet-g'
-    schema = 'de_2506_a'
-    df = get_table(engine=engine, table_name=table_name, schema=schema)
-    gdf = recreate_geometry(df)
+    
+    if os.path.isfile(cache_path):
+        gdf = geopandas.read_parquet(cache_path)  
+    else:
+        engine = db_engine()
+        table_name = 'earthquakes-svet-g'
+        schema = 'de_2506_a'
+        df = get_table(engine=engine, table_name=table_name, schema=schema)
+        gdf = recreate_geometry(df)
+        gdf.to_parquet(cache_path)
     return gdf
 
 if __name__ == '__main__':
-    print(extract())
+    extract()
