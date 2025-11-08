0658c843a61fea2d8035a7796408276c78b74578
svet-g <svetlana.gljadelkina@gmail.com>
2025-11-08 16:39:05 +0000

    Refactor extract function to remove caching logic
    Removed caching logic for parquet file and always fetch data from the database.

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -6,19 +6,12 @@ import pandas as pd
 sys.path.append(str(Path(__file__).parent.parent.parent / "src"))
 from utils.extract_utils import db_engine, get_table
 
-cache_path = Path(__file__).parent.parent.parent / 'data' / 'earthquakes.parquet'
-
 def extract():
-    
-    if os.path.isfile(cache_path):
-        df = pd.read_parquet(cache_path)  
-    else:
-        engine = db_engine()
-        table_name = 'earthquakes-svet-g'
-        schema = 'de_2506_a'
-        df = get_table(engine=engine, table_name=table_name, schema=schema)
-        # gdf = recreate_geometry(df)
-        df.to_parquet(cache_path)
+    engine = db_engine()
+    table_name = 'earthquakes-svet-g'
+    schema = 'de_2506_a'
+    df = get_table(engine=engine, table_name=table_name, schema=schema)
+    # gdf = recreate_geometry(df)
     return df
 
 if __name__ == '__main__':
