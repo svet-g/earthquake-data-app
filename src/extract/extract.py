3f3f131f6f272a3b6592db3b3adc4a04d35f94e4
svet-g <svetlana.gljadelkina@gmail.com>
2025-11-08 16:42:50 +0000

    Integrate Streamlit caching in extract function
    Added Streamlit caching to the extract function.

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -3,9 +3,11 @@ from pathlib import Path
 import sys
 import geopandas
 import pandas as pd
+import streamlit as st
 sys.path.append(str(Path(__file__).parent.parent.parent / "src"))
 from utils.extract_utils import db_engine, get_table
 
+@st.cache_data(ttl=86400)
 def extract():
     engine = db_engine()
     table_name = 'earthquakes-svet-g'
